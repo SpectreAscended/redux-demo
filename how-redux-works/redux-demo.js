0224cee026407f26b0e5683984f529b89cb25357
Cory Pollard <astandard87@gmail.com>
2023-01-27 10:26:34 -0600

    first commit

---
+++ how-redux-works/redux-demo.js
@@ -0,0 +1,8 @@
+const redux = require('redux');
+
+// Reducer function - Should be a pure function.  Same input leads to the same output.  There should be no side effects, so no writing to local storage, making an api call etc...
+// Inputs: Old State + Dispatched Action  ---> Output: New state object.
+
+const counterReducer = () => {};
+
+const store = redux.createStore();
