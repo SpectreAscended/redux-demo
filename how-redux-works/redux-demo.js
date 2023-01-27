869a087138f0198f7db51f9f87c1eafa5c9c94c0
Cory Pollard <astandard87@gmail.com>
2023-01-27 10:38:46 -0600

    Added notes on redux

--- how-redux-works/redux-demo.js
+++ how-redux-works/redux-demo.js
@@ -3,6 +3,27 @@ const redux = require('redux');
 // Reducer function - Should be a pure function.  Same input leads to the same output.  There should be no side effects, so no writing to local storage, making an api call etc...
 // Inputs: Old State + Dispatched Action  ---> Output: New state object.
 
-const counterReducer = () => {};
+// .getState() is a method in store and will give us the latest state snapshop after it was updated.
 
-const store = redux.createStore();
+// subscribe() is a method in store that we use to execute our subscriber function that, here, we define as counterSubscriber.
+
+// dispatch() - We give this an object with a type.  In its simplest form, that is all we need to give it.  Similar to useReducer.
+
+// Our reducer runs once when it is initialized, and again when we dispatch an action to it.
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  return {
+    counter: state.counter + 1,
+  };
+};
+
+const store = redux.createStore(counterReducer);
+
+const counterSubscriber = () => {
+  const latestState = store.getState();
+  console.log(latestState);
+};
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({ type: 'INCREMENT' });
